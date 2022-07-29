a88523f8c449aa5490ae904cb2206d039eaed9a8
anitaalam <77129518+anitaalam@users.noreply.github.com>
2022-07-28 18:35:15 -0700

    Navigation and Banner
    Created the navigation and banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.375rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      '9xl': '8rem'
+    },
     extend: {
-      backgroundImage: {
-        'home-banner': "url('/images/home-banner.png",
-      }
+      // dropShadow: {
+      //   '3xl': '4px 4px 4px rgba(0, 0, 0, 0.55)',
+      // },
+      colors: {
+        'blue': '#1fb6ff',
+        'ice-green': '#00efb6',
+        'ice-orange': "#ffa658"
+      },
+      fontFamily: {
+        'sans': ['Roboto Condensed'],
+        'sans': ['Anton'],
+      },
     },
   },
   plugins: [],
